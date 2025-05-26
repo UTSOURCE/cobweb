7d167f25099a68d214fbaf35cf0ddf1c465bdca1
shynome <shynome@gmail.com>
2025-05-26 08:25:56 +0800

    修复 users 表可以任意创建用户的问题

---
+++ migrations/1748219004_users.go
@@ -0,0 +1,24 @@
+package migrations
+
+import (
+	"fmt"
+
+	"github.com/pocketbase/pocketbase/core"
+	"github.com/pocketbase/pocketbase/migrations"
+	"github.com/shynome/err0"
+	"github.com/shynome/err0/try"
+)
+
+func init() {
+	migrations.Register(func(app core.App) (err error) {
+		defer err0.Then(&err, nil, nil)
+
+		users := try.To1(app.FindCollectionByNameOrId("users"))
+		users.CreateRule = nil // 禁止创建用户
+		try.To(app.SaveNoValidate(users))
+
+		return nil
+	}, func(app core.App) error {
+		return fmt.Errorf("users disable create rollback todo")
+	})
+}
